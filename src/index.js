fe1c879ad53d8a240425e8cf2d0bf1e04e539205
MarkByh <123231312fh@gmail.com>
2023-02-25 14:48:18 +0200

    gf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import ImgApiServise from './loadImg';
 
-
+import SimpleLightbox from "simplelightbox";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import renderGaleryItem from './renderGalery';
 
@@ -18,7 +18,7 @@ loadBtn.classList.add("visually-hidden");
 
 
 function onSearch(event){
-   
+
     loadBtn.classList.add("visually-hidden");
     // galleryShow.refresh();
     event.preventDefault();
@@ -26,22 +26,19 @@ function onSearch(event){
     ImgLoad.resetP();
     ImgLoad.query = event.currentTarget.searchQuery.value.trim();;
     
-    
     if(ImgLoad.searchQuery === ''){
         return Notify.info(`Please, enter what you want to search`);
       }
-      
       ImgLoad.getGallery().then(({ hits, totalHits })=>{
         if(!hits.length) {
-            return Notify.info(`Sorry, there are no images matching your search query. Please try again.`);
+            return Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
         }
         loadBtn.classList.remove('visually-hidden');
         renderGaleryItem(hits);
         Notify.info(`Hooray! We found ${totalHits} images.`);
             if (hits.length === totalHits) {
                 loadBtn.classList.add("visually-hidden");
-                Notify.info(`We're sorry, but you've reached the end of search results.`);
-                    
+                Notify.info(`We're sorry, but you've reached the end of search results.`);   
                 }
                 loadBtn.disabled = false;
                 loadBtn.textContent = "Load more";
@@ -56,16 +53,13 @@ function onBtnClick(event) {
     loadBtn.disabled = true;
     loadBtn.textContent = "Load more";
     loadBtn.classList.remove('visually-hidden');
-
     ImgLoad.getGallery().then(({ hits, totalHits }) => {
         ImgLoad.addloadEments(hits);
         loadBtn.disabled = false;
         let lastToload = totalHits - ImgLoad.loadedEments;
           if (lastToload === 0) {
-            
             loadBtn.classList.add("visually-hidden");
                 Notify.info(`We're sorry, but you've reached the end of search results.`);
-            
           }
          if (lastToload<40) {
             ImgLoad.setNewperPage(lastToload);
